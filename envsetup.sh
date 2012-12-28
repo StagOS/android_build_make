676a27114e59ad3dc7840f1eb884870208627bde
nebkat <nebkat@teamhacksung.org>
2012-12-28 10:40:45 +0000

    cout: move to $out
       * Bring back from the dead
    
    Signed-off-by: Shekhawat2 <anandsingh215@yahoo.com>

--- envsetup.sh
+++ envsetup.sh
@@ -9,6 +9,7 @@ Invoke ". build/envsetup.sh" from your shell to add the following functions to y
              build, and stores those selections in the environment to be read by subsequent
              invocations of 'm' etc.
 - tapas:     tapas [<App1> <App2> ...] [arm|x86|mips|arm64|x86_64|mips64] [eng|userdebug|user]
+- cout:      Changes directory to out.
 - croot:     Changes directory to the top of the tree.
 - m:         Makes from the top of the tree.
 - mm:        Builds all of the modules in the current directory, but not their dependencies.
@@ -955,6 +956,15 @@ function croot()
         fi
     else
         echo "Couldn't locate the top of the tree.  Try setting TOP."
+    fi
+}
+
+function cout()
+{
+    if [  "$OUT" ]; then
+        cd $OUT
+    else
+        echo "Couldn't locate out directory.  Try setting OUT."
     fi
 }
 

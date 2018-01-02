f2d85b93a67fe7d7eb123e56448d13defb343a64
maxwen <max.weninger@gmail.com>
2018-01-02 23:22:06 +0100

    [1/2] build: add OTA script do delete package cache
    Change-Id: I6991aae2360ced58bbebd661a81b2d2c50c5467a
    Signed-off-by: Josh Fox(XlxFoXxlX) <joshfox87@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -1048,6 +1048,10 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     script.ShowProgress(0.02, 10)
     script.RunBackup("restore", sysmount)
 
+  script.Mount("/system")
+  script.RunCleanCache()
+  script.Unmount("/system")
+
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
 

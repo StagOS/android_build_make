9e3cb4525d0e88795385ae2d897d6ab2ffbafa8d
Adrian DC <radian.dc@gmail.com>
2018-05-12 11:24:33 +0200

    build: Include StagOS specific properties in build.prop
     * Our properties were supposed to go to /system/etc/prop.default
        after the following commit:
        "pixys: Move to Google's method of defining system default props"
        Change-Id: I6cb0e28a7599b010b389cc541015a37010a00f4b
    
     * However if BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED is not true,
        only /default.prop will retain the properties contents of
        ADDITIONAL_DEFAULT_PROPERTIES and PRODUCT_SYSTEM_DEFAULT_PROPERTIES,
        and none of our versioning identification was held in the system
    
     * Enabling BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED globally would
        break all properties on devices that handle partitions usually
        at the device level rather than the kernel due to mounting races
    
     * Include the vendor/pixys/config/main.mk entrypoint to be allowed
        to extend ADDITION_BUILD_PROPERTIES as we need to define our releases
    
    Change-Id: I19918eece0f0dd3ee967db0339b4d09c9a6d540e
    Signed-off-by: Adrian DC <radian.dc@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- core/main.mk
+++ core/main.mk
@@ -464,6 +464,10 @@ BUILD_WITHOUT_PV := true
 ADDITIONAL_SYSTEM_PROPERTIES += net.bt.name=Android
 
 # ------------------------------------------------------------
+# Include vendor specific additions to build properties
+-include vendor/stag/build/core/main.mk
+
+# ------------------------------------------------------------
 # Define a function that, given a list of module tags, returns
 # non-empty if that module should be installed in /system.
 

ezio84/abc_build

Fix conditionalization for SystemUIGoogle
Change-Id: I16ccc492b1f316e8a3e553acdd43ce392b8ceef7

target/product/handheld_product.mk
@@ -41,10 +41,13 @@ PRODUCT_PACKAGES += \
     WallpaperCropper \
     frameworks-base-overlays
 
-ifeq ($(filter blueline bonito coral crosshatch flame sargo taimen walleye,$(du_device)),)
-PRODUCT_PACKAGES += SystemUIGoogle
+SYSTEM_UI_PACKAGE :=
+ifneq ($(filter blueline bonito coral crosshatch flame sargo taimen walleye,$(ABC_BUILD)),)
+SYSTEM_UI_PACKAGE := SystemUIGoogle
+PRODUCT_PACKAGES += $(SYSTEM_UI_PACKAGE)
 else
-PRODUCT_PACKAGES += SystemUI
+SYSTEM_UI_PACKAGE := SystemUI
+PRODUCT_PACKAGES += $(SYSTEM_UI_PACKAGE)
 endif
 
 PRODUCT_PACKAGES_DEBUG += \

StagOS/android_build_make

target/product/handheld_product.mk
@@ -31,11 +31,14 @@ PRODUCT_PACKAGES += \
     WallpaperCropper \
     frameworks-base-overlays
 
-ifeq ($(filter blueline bonito coral crosshatch flame sargo taimen walleye,$(du_device)),)
-PRODUCT_PACKAGES += SystemUIGoogle
+SYSTEM_UI_PACKAGE :=
+ifneq ($(filter blueline bonito coral crosshatch flame sargo taimen walleye,$(DU_BUILD)),)
+SYSTEM_UI_PACKAGE := SystemUIGoogle
 else
-PRODUCT_PACKAGES += SystemUI
+SYSTEM_UI_PACKAGE := SystemUI
 endif
+
+PRODUCT_PACKAGES += $(SYSTEM_UI_PACKAGE)
 
 PRODUCT_PACKAGES_DEBUG += \
     frameworks-base-overlays-debug
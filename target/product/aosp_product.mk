793e154d120f545fae2eb711d5abaef714cc79be
vjspranav <pranavasri@live.in>
2022-02-13 19:52:42 +0530

    Enable AllAudio fpr all builds

--- target/product/aosp_product.mk
+++ target/product/aosp_product.mk
@@ -19,11 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
 # Default AOSP sounds
-ifeq ($(STAG_BUILD),)
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
-else
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
-endif
 
 # Additional settings used in all AOSP builds
 PRODUCT_PRODUCT_PROPERTIES += \
